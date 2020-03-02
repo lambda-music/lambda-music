5b78aecaeab2573205c0f194bfbb584fc74dc69b
Ats <oka.ats@gmail.com>
2020-03-02 21:30:53 +0900

    removed unnecessary files

--- workspace/pulsar/compile-scheme.sh
+++ workspace/pulsar/compile-scheme.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# This shell script was an experimental attempt to precompile Scheme files
+# which was not actually used in the application. This remains here by a 
+# historical reason.
+
 shopt -s globstar
 
 cd src.scheme
 
 kawa -d ../bin.scheme/ -C ./**/*.scm
+
