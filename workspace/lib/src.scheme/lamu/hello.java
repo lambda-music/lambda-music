4ab54d68caeab4473022c2dde17ddb8cefc2e446
Ats <oka.ats@gmail.com>
2020-04-07 09:07:49 +0900

    added a demonstration for the @SourceName annotaion

---
+++ workspace/lib/src.scheme/lamu/hello.java
@@ -0,0 +1,45 @@
+package lamu;
+
+import gnu.expr.SourceName;
+import gnu.mapping.Procedure0;
+
+/**
+ * This class demonstrates so-called 'mangling' which is described in
+ * the <a href="https://www.gnu.org/software/kawa/Mangling.html">documentation</a>.  
+ *<pre> 
+ * (import (lamu hello))
+ * => (hello-world-91) is available
+ * 
+ * ((lamu.hello):hello-world-91 )
+ * => error
+ * 
+ * ((lamu.hello):fooBar )
+ * => available
+ * 
+ * ((lamu.hello):foo-bar )
+ * => error
+ *
+ * ((lamu.hello):hello-world-foo-bar )
+ * => available
+ * </pre>
+ * 
+ */
+public class hello {
+    @SourceName( name = "hello-world-91" )
+    public static Procedure0 helloWorld = new Procedure0() {
+        @Override
+        public Object apply0() throws Throwable {
+            return "hello world";
+        }
+    }; 
+    public Procedure0 fooBar = new Procedure0() {
+        @Override
+        public Object apply0() throws Throwable {
+            return "hello world";
+        }
+    };
+    
+    public String helloWorldFooBar() {
+        return "foo";
+    }
+}
