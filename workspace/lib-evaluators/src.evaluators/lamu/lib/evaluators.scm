2c04faef03cb4132687a27594ffd1dc1ca004f24
Ats <oka.ats@gmail.com>
2020-06-24 19:35:22 +0900

    [recompiled] v20200624

--- workspace/lib-evaluators/src.evaluators/lamu/lib/evaluators.scm
+++ workspace/lib-evaluators/src.evaluators/lamu/lib/evaluators.scm
@@ -1,12 +1,16 @@
 
-(define (defined? k) 
+(define (defined?-proc k) 
   (environment-bound? (interaction-environment) k ))
 
-(define (define-if-not-exists k v)
-  (if (defined? k)
+(define (defined-if-not-defined-proc k v)
+  (if (defined?-proc k)
       #f
       (eval (list 'define k (list 'quote v)))))
 
+(define-macro defined?
+  (lambda (k)
+    `(defined?-proc ',k)))
+
 (define-macro define-if-not-defined
   (lambda (k v)
-    `(define-if-not-exists ',k ,v)))
+    `(defined-if-not-defined-proc ',k ,v)))
